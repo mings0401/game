dfbc04c110221aa57e1270e1f6387e4e4808b8d1
Minkyujo <minkyujo@irm.kr>
2016-11-30 23:22:31 +0900

    야구 게임 부터 만들기 시작!
    activity만 구성해 놓음

--- app/src/main/java/com/sunming/gamefactory/MainActivity.java
+++ app/src/main/java/com/sunming/gamefactory/MainActivity.java
@@ -1,13 +1,38 @@
 package com.sunming.gamefactory;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
+    Button bullsandcows;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bullsandcows = (Button)findViewById(R.id.bullsandcows_btn);
+        View.OnClickListener moveGame = new View.OnClickListener() {
+            public void onClick(View v) {
+                switch(v.getId()){
+                    case R.id.bullsandcows_btn :
+                    Intent reserveintent = new Intent(MainActivity.this, BullsAndCowsGameActivity.class);
+                    startActivity(reserveintent);
+                        break;
+
+                };
+            }
+
+        };
+
+        bullsandcows.setOnClickListener(moveGame);
+
     }
+
+
+
+
 }
