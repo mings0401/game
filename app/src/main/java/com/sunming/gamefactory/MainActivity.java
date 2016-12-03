3c3f1075b16b886b338b8de85a5a1196c8cd60a3
Minkyujo <minkyujo@irm.kr>
2016-12-03 17:08:17 +0900

    Start 버튼 만듬

--- app/src/main/java/com/sunming/gamefactory/MainActivity.java
+++ app/src/main/java/com/sunming/gamefactory/MainActivity.java
@@ -1,25 +1,47 @@
 package com.sunming.gamefactory;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.InterstitialAd;
 
 public class MainActivity extends Activity {
+    public static Context mContext; //다른 activity에서 함수 호출되도록
     private AdView [] mAdView = new AdView[2]; //광고 담을 변수
+    private TextView missionNumberTextView, countNumberTextView, stateTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mContext = this;
+
         settingAd(); //광고 호출 함수
 
+        //TextView 변수 초기화
+        missionNumberTextView = (TextView) findViewById(R.id.mission_txt); //좌측 상단 미션 숫자
+        countNumberTextView = (TextView) findViewById(R.id.count_txt); //가운데 쓰레드로 변경될 숫자
+        stateTextView = (TextView) findViewById(R.id.stage_txt); // 상단 가운데 Stage Number
+
+        Intent startDailogIntent = new Intent(getApplicationContext(), StartDialogActivity.class);
+        startActivity(startDailogIntent);
+
+    }
+
+    /**
+     * 게임 시작
+     */
+    public void startGame(){
+//        Toast.makeText(getApplicationContext(), "토스트메시지입니다.", Toast.LENGTH_LONG).show();
     }
 
     /**
---
+++ app/src/main/java/com/sunming/gamefactory/StartDialogActivity.java
@@ -0,0 +1,32 @@
+package com.sunming.gamefactory;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+public class StartDialogActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setFinishOnTouchOutside(false);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_start_dialog);
+
+        Button startClick50Btn = (Button)findViewById(R.id.startClick50Btn);
+        startClick50Btn.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity)MainActivity.mContext).startGame();
+                finish();
+            }
+        });
+    }
+
+}
